673d41ff6b8820b4552e33ed8c14ec3acb7d7a6e
aleksander73 <olekdobkowski@gmail.com>
2021-01-14 03:12:05 +0100

    release-v1.0 (#1)
    * Say hello
    * Read project name
    * Build project structure
    * Create main.cpp
    * Create CMakeLists.txt
    * Create compile.sh
    * Create run.sh
    * Open in Visual Studio Code
    * Elaborates on README.md

---
+++ cpp-ignite.sh
@@ -0,0 +1,43 @@
+echo "C++ project ignite for Linux"
+read -p "Project name: " project_name
+project_name=$(echo $project_name | tr -d '\r')
+
+mkdir $project_name
+cd $project_name
+mkdir build
+mkdir include
+mkdir src
+
+# main.cpp
+cat << EOF > main.cpp
+#include <iostream>
+int main(int argc, char* argv[]) {
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
+EOF
+
+# CMakeLists.txt
+cat << EOF > CMakeLists.txt
+cmake_minimum_required(VERSION 3.5)
+include_directories(include)
+file(GLOB_RECURSE SOURCES main.cpp "src/*.cpp")
+add_executable($project_name \${SOURCES})
+EOF
+
+# compile.sh
+cat << EOF > compile.sh
+cd build
+cmake ..
+make
+EOF
+chmod +x compile.sh
+
+# run.sh
+cat << EOF > run.sh
+cd build
+./${project_name}
+EOF
+chmod +x run.sh
+
+code .
